578fdab79c050f23605ff3b0ffaaf250bd8d6a9d
Lam1ro4eQ <alsajgushev@gmail.com>
2021-10-27 10:34:20 +0300

    lessons codewars

--- codewars/script.js
+++ codewars/script.js
@@ -13,4 +13,32 @@ function squareDigits(num){
   return out;
 }
 
-squareDigits(9123);
+
+function squareDigits(num){
+  let txtArr = String(num).split('');
+  let out = '';
+  for ( let i = 0; txtArr.length > i; i++ ) {
+    out += Math.pow(txtArr[i], 2);
+  }
+  out = Number(out);
+  console.log(typeof(out));
+  console.log(out);
+  return out;
+}
+
+// squareDigits(9123);
+
+
+
+
+// Given: an array containing hashes of names
+// Return: a string formatted as a list of names separated by commas except for the last two names, which should be separated by an ampersand.
+
+function list(names){
+  for ( let i = 0; names.length > i; i++ ) {
+    1
+  }
+}
+
+list([ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ])
+// returns 'Bart, Lisa & Maggie'
