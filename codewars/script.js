73d4a476e6555121caba6683c154af965fbfd579
Lam1ro4eQ <alsajgushev@gmail.com>
2021-10-28 19:22:53 +0300

    codewars

--- codewars/script.js
+++ codewars/script.js
@@ -101,4 +101,31 @@ function printerError(s) {
   return res;
 }
 
-printerError('aaabbbbhaijjjm');
+// printerError('aaabbbbhaijjjm');
+
+
+
+
+
+// Вы, наверное, знаете систему «лайков» по ​​Facebook и другим страницам. Люди могут "лайкать" сообщения в блогах, изображения или другие предметы. Мы хотим создать текст, который должен отображаться рядом с таким элементом.
+// Реализуйте функцию, которая принимает массив, содержащий имена людей, которым нравится предмет. Он должен возвращать отображаемый текст, как показано в примерах:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+function likes(names) {
+  if (names.length == 0) {
+    return "no one likes this";
+  } else if (names.length == 1) {
+    return names[0] + " likes this";  
+  } else if (names.length == 2) {
+    return names[0] + " and " + names[1] + " like this";
+  } else if (names.length == 3) {
+    return names[0] + ", " + names[1] + " and " + names[2] + " like this";
+  } else {
+    return names[0] + ", " + names[1] + " and " + (names.length - 2) + " others like this";
+  }
+}
