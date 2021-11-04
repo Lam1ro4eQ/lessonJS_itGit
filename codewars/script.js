4cba7885276aa9a1e036cf2864020b22e10030a2
Lam1ro4eQ <alsajgushev@gmail.com>
2021-11-04 09:39:46 +0300

    codewars new

--- codewars/script.js
+++ codewars/script.js
@@ -530,3 +530,15 @@ function getEvenNumbers(numbersArray){
 function solve(s){
   return Math.max(...s.split(/[^aoeiu]/i).map(s => s.length))
 }
+
+
+
+
+
+// Consider the word "abode". We can see that the letter a is in position 1 and b is in position 2. In the alphabet, a and b are also in positions 1 and 2. Notice also that d and e in abode occupy the positions they would occupy in the alphabet, which are positions 4 and 5.
+// Given an array of words, return an array of the number of letters that occupy their positions in the alphabet for each word. For example,
+
+function solve(arr){  
+  let alphabeth = "abcdefghijklmnopqrstuvwxyz";
+    return arr.map(x => x.toLowerCase().split('').filter((y,i) => i==alphabeth.indexOf(y)).length);
+  };
