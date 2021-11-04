90dad524c837b69ba2c76a7f49e701b4f233d356
Lam1ro4eQ <alsajgushev@gmail.com>
2021-11-04 08:42:49 +0300

    edit

--- codewars/script.js
+++ codewars/script.js
@@ -482,3 +482,8 @@ function invert(array) {
 
 
 
+// Write a function called repeatStr which repeats the given string string exactly n times.
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
