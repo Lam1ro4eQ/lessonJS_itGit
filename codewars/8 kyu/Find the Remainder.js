775c0bb423a9c81c170dd85fe52b276da8db0e04
Lam1ro4eQ <alsajgushev@gmail.com>
2022-10-22 13:37:49 +0300

    8 kyu

---
+++ codewars/8 kyu/Find the Remainder.js
@@ -0,0 +1,12 @@
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+function remainder(n, m){
+    if (n>m && m===0) {
+          return NaN;
+      } else if (m>n && n===0){
+          return NaN;
+      } 
+      return (n < m) ? m%n : n%m;
+  }
